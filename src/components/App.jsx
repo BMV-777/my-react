1cadbe020a5b9341abfd81696a7355425121ba29
Maks Borenkov <borenkovmv@gmail.com>
2022-11-02 11:41:45 +0200

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+// import logo from '../logo.svg';
+// import '../App.css';
+import { PageTitle } from '../components/PageTitle/PageTitle';
+
+export const App = () => {
+  return (
+    <div>
+      <PageTitle text="24th Core Worlds Coalition Conference" />
+    </div>
+  );
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+
+//         <p>HI</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
