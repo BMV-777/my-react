75ad62a263a97256ab944c7db2063f52967642b1
Maks Borenkov <borenkovmv@gmail.com>
2022-11-02 19:51:05 +0200

    commits two 1/2

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -8,6 +8,7 @@ import {
 // import { formatEventStart } from 'utils/formatEventStart';
 // import { formatEventDuration } from 'utils/formatEventDuration';
 import { formatEventDuration, formatEventStart } from 'utils';
+import { iconsSize } from 'contants';
 import css from './Event.module.css';
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
@@ -17,19 +18,19 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
     <div className={css.event}>
       <h2 className={css.title}>{name}</h2>
       <p className={css.info}>
-        <FaMapMarkerAlt className={css.icon} size={16} />
+        <FaMapMarkerAlt className={css.icon} size={iconsSize.sm} />
         {location}
       </p>
       <p className={css.info}>
-        <FaUserAlt className={css.icon} size={16} />
+        <FaUserAlt className={css.icon} size={iconsSize.sm} />
         {speaker}
       </p>
       <p className={css.info}>
-        <FaCalendarAlt className={css.icon} size={16} />
+        <FaCalendarAlt className={css.icon} size={iconsSize.sm} />
         {formateTime}
       </p>
       <p className={css.info}>
-        <FaClock className={css.icon} size={16} />
+        <FaClock className={css.icon} size={iconsSize.sm} />
         {formatDistance}
       </p>
       <span className={`${css.chip} ${css[type]} `}>{type}</span>
